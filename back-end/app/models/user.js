6f18d0c12087c618fd6c3d7c383917ce0259f4eb
ak1103 <ak.apichan1103@gmail.com>
2015-09-26 09:46:52 +0700

    user collection

--- back-end/app/models/user.js
+++ back-end/app/models/user.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
 		local: {
 			username: String,
+			email: String,
 			password: String
 		}
 });
--- back-end/app/routes.js
+++ back-end/app/routes.js
@@ -4,11 +4,12 @@ module.exports = function(app){
 		res.send("Hello world");
 	});
 
-	app.get('/:username/:password', function(req, res){
+	app.get('/:username/:email/:password', function(req, res){
 		var newUser = new User();
 		newUser.local.username = req.params.username;
+		newUser.local.email= req.params.email;
 		newUser.local.password = req.params.password;
-		console.log(newUser.local.username + " " + newUser.local.password);
+		console.log(newUser.local.username + " " + newUser.local.password + " " + newUser.local.email);
 		newUser.save(function(err){
 		if(err)
 			throw err;
